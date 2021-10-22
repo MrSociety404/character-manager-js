3995454620aa6e41070dd6ec63eb574f7e6eeff9
Fabrice <cstfabrice@gmail.com>
2021-10-22 11:53:23 +0200

    Add Card

---
+++ src/components/Card.vue
@@ -0,0 +1,77 @@
+<template>
+  <article class="card">
+    <img
+      class="card__image"
+      :src="`data:image/jpeg;base64,${character.image}`"
+      alt="image"
+    />
+    <div class="card__info">
+      <h2 class="card__title"> {{character.name}} </h2>
+      <p class="card__description"> {{character.shortDescription}} </p>
+      <router-link class="card__link" :to="`/details/${character.id}`">
+        Read More 
+        <box-icon class="card__icon" name="chevron-right"></box-icon>
+      </router-link>
+    </div>
+  </article>
+</template>
+
+<script setup>
+import { defineProps  } from "vue";
+
+defineProps({
+  character: Object
+})
+</script>
+
+<style lang="scss">
+.card {
+  width: 430px;
+  height: 200px;
+  display: flex;
+  align-items: center;
+  background-color: $white;
+  box-shadow: $box-shadow;
+  border-radius: $border-radius;
+  margin: 1em;
+  &__image {
+    height: 100%;
+    min-width: 150px;
+    max-width: 150px;
+    object-fit: cover;
+    border-radius: $border-radius 0 0 $border-radius;
+  }
+  &__info {
+    padding: 1em;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: space-around;
+    height: 100%;
+  }
+  &__title {
+    font-weight: bold;
+    font-size: 1.4em;
+    text-transform: uppercase;
+  }
+  &__description {
+    margin: 1em 0 3em 0 ;
+  }
+  &__link {
+    color: $primary;
+    fill: $primary;
+    font-weight: bold;
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    border-bottom: 2px solid transparent;
+    &:hover {
+      border-bottom: 2px solid $primary ;
+    }
+  }
+  &__icon {
+    width: 20px;
+    height: 20px;
+  }
+}
+</style>
