fb59fa2562cfe4356ffcf57e6fbe3aba48c5d1c5
Maxim K <90226452+coreinside5@users.noreply.github.com>
2021-10-22 14:20:16 +0200

    strangecommit_001

--- src/components/Titlebar.vue
+++ src/components/Titlebar.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    <h1>{{content}}</h1>
+  <div class="title">
+    <h2 class="title__h2">{{content}}</h2>
+    <div class="title__line"></div>
   </div>
 </template>
 
@@ -12,6 +13,23 @@ defineProps({
 })
 </script>
 
-<style>
-    
+<style lang="scss">
+.title{
+    display: flex;
+    align-items: center;
+    &__h2{
+    color: $black;
+    font-size: 2em;
+    font-weight: bold;
+    padding: 1em;
+}
+&__line{
+    height: 2px;
+    width: 160px;
+    background-color:$black;
+}
+   
+}
+
+
 </style>
