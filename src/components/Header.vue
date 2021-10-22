a53cce355d4e6160634107b046581ca06c9a0a3d
Fabrice <cstfabrice@gmail.com>
2021-10-22 10:09:03 +0200

    Add stuff

---
+++ src/components/Header.vue
@@ -0,0 +1,53 @@
+<template>
+  <header class="header">
+    <div class="header__box">
+      <router-link to="/" class="header__box-title">
+        <img class="header__logo" src="@/assets/logo.svg" alt="logo" />
+        <h1 class="header__title">CHARACTER<span>CONTENT MANAGER</span></h1>
+      </router-link>
+      <Button icon="plus" content="ADD NEW" />
+    </div>
+  </header>
+</template>
+
+<script setup>
+import Button from "@/components/Button.vue";
+</script>
+
+<style lang="scss">
+.header {
+  background-color: $white;
+  height: 90px;
+  box-shadow: $box-shadow;
+  &__box {
+    max-width: $xxl;
+    width: 100%;
+    padding: 1em;
+    margin: 0 auto;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    &-title {
+      display: flex;
+      align-items: center;
+      cursor: pointer;
+    }
+  }
+  &__logo {
+    width: 48px;
+    height: auto;
+  }
+  &__title {
+    font-size: 1.8em;
+    font-weight: bold;
+    display: flex;
+    flex-direction: column;
+    margin-left: 0.6em;
+    span {
+      font-weight: normal;
+      font-size: 1rem;
+      letter-spacing: 0.09em;
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
-  <div></div>
+  <div class="home">
+  </div>
 </template>
 
 <script setup>
